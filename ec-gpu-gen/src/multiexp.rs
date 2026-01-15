ff0aa39b99c7dba2faffb66f6052b9dc4c31336e
Fedor Sakharov <fedor.sakharov@gmail.com>
2026-01-15 17:53:19 +0100

    a reference

--- ec-gpu-gen/src/multiexp.rs
+++ ec-gpu-gen/src/multiexp.rs
@@ -559,7 +559,7 @@ where
         let parent_span = tracing::info_span!("parent"); // Use an INFO span for parent
         let _pg = parent_span.enter();
         pool.scoped(|s| {
-            let child_span = tracing::debug_span!("child").or_current();
+            let child_span = tracing::debug_span!(parent: &parent_span, "child").or_current();
             let _span = info_span!("before scoped 2").entered();
             let _entered = child_span.entered();
             results = vec![<G::Group as AdditiveGroup>::ZERO; self.kernels.len()];
